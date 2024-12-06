138a1f120289b4e66c1e9a3273a9baccab691d9b
CRetizRamirez <cretiz.dsr@gmail.com>
2024-12-06 15:12:16 -0600

    Refrescar Token

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { registrarUsuarioController, loginController } from '../controllers/usuarioController.js';
+import { refreshTokenController } from '../controllers/tokenController.js';
 
 const router = express.Router();
 
 router.post("/registrar", registrarUsuarioController);
 router.post("/login", loginController);
+router.post("/refrescar", refreshTokenController);
 
 
 export default router;
